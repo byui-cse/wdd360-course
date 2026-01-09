8b5e2b77594a3892aab138f84a1f23482181226a
Shane T <st83440@gmail.com>
2026-01-09 11:04:22 -0700

    update to Astro5

---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const ponder = defineCollection({ loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/ponder" }), });
+const resources = defineCollection({ loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/resources" }), });
+const units = defineCollection({ loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/units" }), });
+const prepare = defineCollection({ loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/prepare" }), });
+const prove = defineCollection({ loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/prove" }), });
+const teacher = defineCollection({ loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/teacher" }), });
+
+
+
+// 4. Export a single `collections` object to register  your collection(s)
+export const collections = { ponder, resources, units, prepare, prove, teacher };
